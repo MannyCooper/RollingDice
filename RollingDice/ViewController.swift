c406d8c5e7d72d1587f5b6b42a554393fbef39a2
MannyCooper <an1998821@gmail.com>
2021-09-11 23:50:25 -0700

    Rolling Dice Demo
    1. Have a logo
    
    2. Have all the 1x, 2x 3x Images
    
    3. Three buttons, Above 7, below 7 and Lucky 7
    
    4. When user clicks on each button. dice will change randomly and if the user has pressed correct button, Score will go up by 1
    
    5. if lucky 7 is won then points go up by 7 points
    
    6. Have a label which shows score
    
    7. Make sure the app does not go out of bounds on small screens.

--- RollingDice/ViewController.swift
+++ RollingDice/ViewController.swift
@@ -9,9 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgOne: UIImageView!
+    @IBOutlet weak var imgTwo: UIImageView!
+    
+    @IBOutlet weak var totalScore: UILabel!
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func rollDices() -> Array<Int>{
+        let diceOne = Int.random(in: 1...6)
+        let diceTwo = Int.random(in: 1...6)
+        
+        imgOne.image = UIImage(named: "Dice\(diceOne)")
+        imgTwo.image = UIImage(named: "Dice\(diceTwo)")
+        
+        return [diceOne, diceTwo]
+    }
+    
+    @IBAction func below7Pressed(_ sender: Any) {
+        
+        let rollResult = rollDices()
+        
+        if rollResult[0] + rollResult[1] < 7 {
+            score += 1
+        }else{
+            score -= 1
+        }
+        
+        totalScore.text = "Score = \(score)"
+        
+    }
+    @IBAction func lucky7Pressed(_ sender: Any) {
+        
+        let rollResult = rollDices()
+        
+        if rollResult[0] + rollResult[1] == 7 {
+            score += 7
+        }else{
+            score -= 7
+        }
+        
+        totalScore.text = "Score = \(score)"
+        
+    }
+    @IBAction func above7Pressed(_ sender: Any) {
+        let rollResult = rollDices()
+        
+        if rollResult[0] + rollResult[1] > 7 {
+            score += 1
+        }else{
+            score -= 1
+        }
+        
+        totalScore.text = "Score = \(score)"
     }
 
 
